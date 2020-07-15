6f2fe27ce55ed6f7ed7a13fbfeb4aaf2bb0f1fe8
ProCodeGuide <support@procodeguide.com>
2020-07-15 17:16:18 +0530

    First Commit

---
+++ ProCodeGuide.Sample.OptionsPattern/Controllers/AppParamtersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using ProCodeGuide.Sample.OptionsPattern.Models;
+
+namespace ProCodeGuide.Sample.OptionsPattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppParamtersController : ControllerBase
+    {
+        private ApplicationParameters applicationParameters;
+        private ApplicationParameters applicationParametersSnapshot;
+        public AppParamtersController(IOptions<ApplicationParameters> options, IOptionsSnapshot<ApplicationParameters> optionsSnap)
+        {
+            applicationParameters = options.Value;
+            applicationParametersSnapshot = optionsSnap.Value;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            StringBuilder response = new StringBuilder();
+            response.AppendLine("IOptions ==>");
+            response.AppendLine(JsonConvert.SerializeObject(applicationParameters));
+            response.AppendLine("");
+            response.AppendLine("IOptionsSnapshot ==>");
+            response.AppendLine(JsonConvert.SerializeObject(applicationParametersSnapshot));
+            return Content(response.ToString());
+        }
+    }
+}
---
+++ ProCodeGuide.Sample.OptionsPattern/Models/ApplicationParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProCodeGuide.Sample.OptionsPattern.Models
+{
+    public class ApplicationParameters
+    {
+        public string SQLServerConnection { get; set; }
+        public string EmailServer { get; set; }
+        public string ServiceURL { get; set; }
+        public long MaxLimitUsers { get; set; }
+    }
+}
